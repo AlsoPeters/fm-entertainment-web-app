85e8a4ac1b3c38e90cde08fa554e0a25494ef11a
AlsoPeters <Kyle.fire@gmail.com>
2022-10-31 11:16:46 +0900

    add Desktop screen size layout

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,8 +1,8 @@
 const Header = () => {
   return (
-    <nav className="flex h-14 w-auto items-center justify-between bg-ewa-semi-dark-blue px-4 sm:mx-4 sm:mt-4 sm:rounded-lg">
+    <nav className="flex h-14 w-auto items-center justify-between bg-ewa-semi-dark-blue px-4 sm:mx-4 sm:mt-4 sm:rounded-lg lg:h-[960px] lg:w-24 lg:flex-col lg:justify-start lg:py-6">
       <div className="logo h-[20px] w-[25px] bg-ewa-red"></div>
-      <section className="flex gap-4">
+      <section className="flex gap-4 lg:mb-auto lg:mt-16 lg:flex-col lg:gap-10">
         <div className="home h-[16px] w-[16px] bg-ewa-grayish-blue"></div>
         <div className="movies h-[16px] w-[16px] bg-ewa-grayish-blue"></div>
         <div className="tv h-[16px] w-[16px] bg-ewa-grayish-blue"></div>
@@ -16,10 +16,10 @@ const Header = () => {
 
 const Layout = ({ children }) => {
   return (
-    <>
+    <div className="lg:flex">
       <Header />
       {children}
-    </>
+    </div>
   );
 };
 
